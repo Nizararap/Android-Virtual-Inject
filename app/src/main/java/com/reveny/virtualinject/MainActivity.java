9565cb0a2198e2671887cc4b02bc2528c4d29ef6
Nizararap <nizarardhian8@gmail.com>
2026-04-30 12:44:50 +0700

    Upload via GitDeploy

--- app/src/main/java/com/reveny/virtualinject/MainActivity.java
+++ app/src/main/java/com/reveny/virtualinject/MainActivity.java
@@ -1,27 +1,10 @@
 package com.reveny.virtualinject;
 
-import android.os.Bundle;
-import android.util.Log;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.vcore.BlackBoxCore;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+/**
+ * @deprecated Replaced by com.reveny.virtualinject.ui.activity.MainActivity
+ * This file is kept only to avoid Git conflicts. Do not use.
+ */
+@Deprecated
+public class MainActivity {
+    // intentionally empty
 }
